9031b891cc0c143aee78c1f9d88ca580635be98f
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-05-21 14:04:48 +0530

    REFactor: update MUX 2-to-1 implementations to behavioral architecture

--- CODD_nanoprocessor.srcs/sources_1/new/MUX_2to1_3bit.vhd
+++ CODD_nanoprocessor.srcs/sources_1/new/MUX_2to1_3bit.vhd
@@ -2,15 +2,20 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux_2to1_3bit is
-    Port ( a : in STD_LOGIC_VECTOR (2 downto 0);
-           b : in STD_LOGIC_VECTOR (2 downto 0);
-           sel : in STD_LOGIC;
-           y : out STD_LOGIC_VECTOR (2 downto 0));
+    Port ( a   : in  STD_LOGIC_VECTOR(2 downto 0);
+           b   : in  STD_LOGIC_VECTOR(2 downto 0);
+           sel : in  STD_LOGIC;
+           y   : out STD_LOGIC_VECTOR(2 downto 0));
 end mux_2to1_3bit;
 
-architecture gate_level of mux_2to1_3bit is
+architecture behavioral of mux_2to1_3bit is
 begin
-    y(0) <= (a(0) and not sel) or (b(0) and sel);
-    y(1) <= (a(1) and not sel) or (b(1) and sel);
-    y(2) <= (a(2) and not sel) or (b(2) and sel);
-end gate_level;
+    process(a, b, sel)
+    begin
+        if sel = '0' then
+            y <= a;
+        else
+            y <= b;
+        end if;
+    end process;
+end behavioral;
--- CODD_nanoprocessor.srcs/sources_1/new/Mux2to14bit.vhd
+++ CODD_nanoprocessor.srcs/sources_1/new/Mux2to14bit.vhd
@@ -18,32 +18,24 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Mux2to1_4bit is
     Port ( sel : in STD_LOGIC;
-           a : in STD_LOGIC_VECTOR (3 downto 0);
-           b : in STD_LOGIC_VECTOR (3 downto 0);
-           y : out STD_LOGIC_VECTOR (3 downto 0));
+           a   : in STD_LOGIC_VECTOR(3 downto 0);
+           b   : in STD_LOGIC_VECTOR(3 downto 0);
+           y   : out STD_LOGIC_VECTOR(3 downto 0));
 end Mux2to1_4bit;
 
-architecture gate_level of Mux2to1_4bit is
-
+architecture behavioral of Mux2to1_4bit is
 begin
-    y(0) <= (a(0) and not sel) or (b(0) and sel);
-    y(1) <= (a(1) and not sel) or (b(1) and sel);
-    y(2) <= (a(2) and not sel) or (b(2) and sel);
-    y(3) <= (a(3) and not sel) or (b(3) and sel);
-
-end gate_level; 
+    process(a, b, sel)
+    begin
+        if sel = '0' then
+            y <= a;
+        else
+            y <= b;
+        end if;
+    end process;
+end behavioral;
