60a58d42b79d1f830940c1b4603e32e536a2407a
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-05-25 00:28:35 +0530

    reduced LUTs

--- CODD_nanoprocessor.srcs/sources_1/new/Slow_Clk.vhd
+++ CODD_nanoprocessor.srcs/sources_1/new/Slow_Clk.vhd
@@ -1,56 +1,28 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10.04.2025 12:01:08
--- Design Name: 
--- Module Name: Slow_Clk - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY Slow_Clk IS
   PORT (
-    Clk_in : IN STD_LOGIC;
-    Clk_out : OUT STD_LOGIC);
-
+    Clk_in  : IN  STD_LOGIC;
+    Clk_out : OUT STD_LOGIC
+  );
 END Slow_Clk;
 
 ARCHITECTURE Behavioral OF Slow_Clk IS
-
-  SIGNAL count : INTEGER := 1;
-  SIGNAL Clk_status : STD_LOGIC := '0';
-
+  SIGNAL count      : UNSIGNED(2 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL clk_status : STD_LOGIC := '0';
 BEGIN
-
-  PROCESS (Clk_in) BEGIN IF
-    (rising_edge(Clk_in)) THEN
-    count <= count + 1;
-    IF (count = 5) THEN
-      Clk_status <= NOT (Clk_status);
-      Clk_out <= Clk_status;
-      count <= 1;
+  PROCESS (Clk_in)
+  BEGIN
+    IF rising_edge(Clk_in) THEN
+      IF count = "100" THEN  -- 4 in binary, so 0..4 = 5 cycles
+        count      <= (OTHERS => '0');
+        clk_status <= NOT clk_status;
+      ELSE
+        count <= count + 1;
+      END IF;
     END IF;
-  END IF;
-END PROCESS;
+  END PROCESS;
+  Clk_out <= clk_status;
 END Behavioral;
