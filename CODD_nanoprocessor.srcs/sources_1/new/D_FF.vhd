e8dc7234d4b686631435f6cd1d17868fa4895d64
TharakaUJ <9dmpires2k17.tuj@gmail.com>
2025-05-26 21:24:04 +0530

    Fix D_FF process to use falling edge of Clk and update WTXSimLaunchSim option in project configuration

--- CODD_nanoprocessor.srcs/sources_1/new/D_FF.vhd
+++ CODD_nanoprocessor.srcs/sources_1/new/D_FF.vhd
@@ -13,7 +13,7 @@ architecture Behavioral of D_FF is
 begin
     process (Clk)
     begin
-        if rising_edge(Clk) then
+        if falling_edge(Clk) then
             if Res = '1' then
                 Q <= '0';
             elsif En = '1' then
